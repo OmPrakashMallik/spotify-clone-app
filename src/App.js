238fb915854e3ed1498566609ba0676502260723
Om Prakash Mallik <omprakashmallik91@gmail.com>
2023-04-21 00:39:46 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+
+function App() {
+  const [token, setToken] = useState(null);
+
+  useEffect(()=> {
+    const token = getTokenFromUrl();
+    window.location.hash = "";
+    const _token = hash.access_token;
+
+    if(token) {
+      setToken(_token)
+    }
+
+    console.log("I HAVE A TOKEN>>>", token);
+  }, []);
+
+  return (
+    // BEM
+    <div className="app"> 
+      {
+        token ? (
+          <Player/>
+        ) : (
+          <Login/>
+        )
+      }
+      <Login/>
+    </div>
+
+  );
+}
+
+export default App;
